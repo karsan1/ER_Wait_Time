dd4ee01efaeb3bb0c047c6888657c0ee9b4aaf0e
karsan1 <98597909+karsan1@users.noreply.github.com>
2023-04-08 00:20:28 -0400

    Add files via upload

---
+++ Er_Wait/Er_Wait/Er_WaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Er_WaitApp.swift
+//  Er_Wait
+//
+//  Created by Karthick Sankar on 4/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Er_WaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
